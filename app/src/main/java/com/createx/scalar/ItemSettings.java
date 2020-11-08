999d1cb2d59194ace0ac9899142f38ccf010382d
A'maya Solomon <amayasolomon@Amayas-MacBook-Pro.local>
2020-11-08 03:12:57 -0500

    more additions

---
+++ app/src/main/java/com/createx/scalar/ItemSettings.java
@@ -0,0 +1,13 @@
+package com.createx.scalar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ItemSettings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_settings);
+    }
+}
--- app/src/main/java/com/createx/scalar/MainActivity.java
+++ app/src/main/java/com/createx/scalar/MainActivity.java
@@ -1,5 +1,9 @@
 package com.createx.scalar;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,6 +15,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // To Sign Up Screen
+        signUpButton = findViewById(R.id.signUpButton);
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nextScreen();
+            }
+        });
+    }
+
+    /**
+     * Method that switches between activities
+     */
+    public void nextScreen() {
+        Intent intent = new Intent(MainActivity.this, SignUp.class);
+        startActivity(intent);
     }
 
 
