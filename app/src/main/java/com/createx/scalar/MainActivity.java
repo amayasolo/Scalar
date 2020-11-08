d3a0d4d4b522eadbd0b0b15e8ee56332d315ec87
A'maya Solomon <amayasolomon@Amayas-MacBook-Pro.local>
2020-11-08 03:33:04 -0500

    n

--- app/src/main/java/com/createx/scalar/MainActivity.java
+++ app/src/main/java/com/createx/scalar/MainActivity.java
@@ -18,19 +18,21 @@ public class MainActivity extends AppCompatActivity {
 
         // To Sign Up Screen
         signUpButton = findViewById(R.id.signUpButton);
+        final Class signup = SignUp.class;
         signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                nextScreen();
+                nextScreen(signup);
             }
         });
     }
 
     /**
      * Method that switches between activities
+     * @param screen next screen
      */
-    public void nextScreen() {
-        Intent intent = new Intent(MainActivity.this, SignUp.class);
+    public void nextScreen(Class screen) {
+        Intent intent = new Intent(MainActivity.this, screen);
         startActivity(intent);
     }
 
