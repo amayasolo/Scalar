d566b5cc4a71ea7eec8860385a5d07ac232aec7f
A'maya Solomon <amayasolomon@Amayas-MacBook-Pro.local>
2020-11-08 02:18:12 -0500

    updating

--- app/src/main/java/com/createx/scalar/MainActivity.java
+++ app/src/main/java/com/createx/scalar/MainActivity.java
@@ -4,10 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private Button signUpButton;
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
 }
