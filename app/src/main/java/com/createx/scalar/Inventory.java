90d3e99fc001bbcbfed42fbacf52f98f692722ca
kgruduah3 <55367731+kgruduah3@users.noreply.github.com>
2020-11-11 12:52:25 -0500

    dis me

--- app/src/main/java/com/createx/scalar/Inventory.java
+++ app/src/main/java/com/createx/scalar/Inventory.java
@@ -1,15 +1,28 @@
 package com.createx.scalar;
 
 import android.content.Intent;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class Inventory extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inventory);
+        ArrayList<String> items = new ArrayList<>();
+        items.add("MILK");
+        items.add("BUTTER");
+        items.add("ORANGE JUICE");
+        items.add("EGG");
+        ListView inventoryList = (ListView) findViewById(R.id.inventoryList);
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.activity_list_item, items);
+        inventoryList.setAdapter(arrayAdapter);
     }
 
     /**
