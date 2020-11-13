37ffa18db56fde342ec88fcb5e4a4acdbd247b90
A'maya Solomon <amayasolomon@Amayas-MacBook-Pro.local>
2020-11-12 20:09:35 -0500

    Scale

--- app/src/main/java/models/Scale.java
+++ app/src/main/java/models/Scale.java
@@ -1,5 +1,47 @@
 package models;
 
 public class Scale {
+    private String ID;
+    private String name;
+    private double initialWeight;
+    private double currentWeight;
+    private double percentage;
 
+    public Scale(String ID, String name) {
+        this.ID = ID;
+        this.name = name;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setInitialWeight(double initialWeight) {
+        if (initialWeight >= 0) {
+            this.initialWeight = initialWeight;
+        }
+    }
+
+    public void setCurrentWeight(double currentWeight) {
+        if (currentWeight >= 0) {
+            this.currentWeight = currentWeight;
+        }
+    }
+
+    public double getPercentage() {
+        percentage = currentWeight/initialWeight;
+        return percentage;
+    }
 }
